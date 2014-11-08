10526916a5fa4f3fd03650d64c132b74e418e0b5
Nengrong Qu(Neo Qu) <Quner8@gmail.com>
2014-11-08 18:14:15 +0800

    complete ui, and add the convert functions

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,48 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->lineEdit_dec->setValidator(new QIntValidator());
+    QRegExp reHex("(0[Xx])?[A-Fa-f0-9]*");
+    ui->lineEdit_hex->setValidator(new QRegExpValidator(reHex));
+    ui->checkBox_autoConvert->toggled(true);
+    ui->pushButtonConvertDec2Hex->setFocus();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_checkBox_autoConvert_toggled(bool checked)
+{
+    ui->pushButtonConvertDec2Hex->setVisible(!checked);
+    ui->pushButtonConvertHex2Dec->setVisible(!checked);
+}
+
+void MainWindow::on_pushButtonConvertDec2Hex_clicked()
+{
+    QString hex = QString::number(ui->lineEdit_dec->text().toLongLong(NULL, 10), 16);
+    ui->lineEdit_hex->setText("0X" + hex.toUpper());
+}
+
+void MainWindow::on_pushButtonConvertHex2Dec_clicked()
+{
+    QString dec = QString::number(ui->lineEdit_hex->text().toLongLong(NULL, 16));
+    ui->lineEdit_dec->setText(dec.toUpper());
+}
+
+void MainWindow::on_lineEdit_dec_textChanged(const QString &arg1)
+{
+    if (ui->checkBox_autoConvert->isChecked())
+    {
+        on_pushButtonConvertDec2Hex_clicked();
+    }
+}
+
+void MainWindow::on_lineEdit_hex_textChanged(const QString &arg1)
+{
+    if (ui->checkBox_autoConvert->isChecked())
+    {
+        on_pushButtonConvertHex2Dec_clicked();
+    }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -15,6 +15,17 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_checkBox_autoConvert_toggled(bool checked);
+
+    void on_pushButtonConvertDec2Hex_clicked();
+
+    void on_pushButtonConvertHex2Dec_clicked();
+
+    void on_lineEdit_dec_textChanged(const QString &arg1);
+
+    void on_lineEdit_hex_textChanged(const QString &arg1);
+
 private:
     Ui::MainWindow *ui;
 };
